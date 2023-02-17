27cd3f92bd7c8c528c813b5e985ab94671aae406
farhan_tanvir <faru02135@gmail.com>
2023-02-17 19:30:48 +0600

    3rd class

---
+++ Class_03/main.go
@@ -0,0 +1,23 @@
+package main 
+
+import "fmt"
+
+func main(){
+
+var name , name2, city string= "FARHAN", "MASTER-ACADEMY", "DHAKA"
+  fmt.Println(name ,name2, city)
+
+ var c rune = 'A'
+ var age int = 19
+ var result float32 = 50.54
+
+  fmt.Println(c, age, result)
+
+
+
+
+
+
+
+
+}
