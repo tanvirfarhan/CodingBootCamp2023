6b5da105a2628ad4cf7613c5d32cefc63f632416
farhan_tanvir <faru02135@gmail.com>
2023-02-24 00:28:06 +0600

    array

---
+++ Class_10/main.go
@@ -0,0 +1,24 @@
+package main 
+
+import "fmt" 
+ 
+func main (){
+
+
+var students [3] string
+
+
+
+
+   students [0] ="farhan"
+
+   students [1] ="tanvir"
+
+   students [2] ="atul" 
+  
+fmt.Println (students)
+
+
+
+
+}
