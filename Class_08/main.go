7ceae9e0a4e46b74b5cb6ca3c453b352ff30a5b0
farhan_tanvir <faru02135@gmail.com>
2023-05-18 08:34:39 +0600

    first commit

---
+++ Class_08/main.go
@@ -0,0 +1,18 @@
+package main 
+
+import "fmt"
+
+func main( ){
+
+var name string = " FARHAN TANVIR"
+var name2 = "Again start"
+//var name1 char ="A,B,C"
+//var name2 int ="1,2,3"
+//var name3 float ="2.14,305.852"
+//var name4 datetime ="10-05-24"
+  
+
+  fmt.Println(name, name2)
+
+
+     }
